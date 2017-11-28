95e960b414b4ddf805ae665c445847e09550845a
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-28 14:51:21 -0500

    Extract rulesUrl variable

--- bot.js
+++ bot.js
@@ -4,10 +4,9 @@ const { JSDOM } = require("jsdom");
 module.exports = botBuilder(function(request) {
   const ruleNumber = Number.parseInt(request.text);
   const ruleIndex = ruleNumber - 1;
+  const rulesUrl = "http://www.point83.com/tos/index.php?title=Basic_rules";
 
-  return JSDOM.fromURL(
-    "http://www.point83.com/tos/index.php?title=Basic_rules"
-  ).then(dom => {
+  return JSDOM.fromURL(rulesUrl).then(dom => {
     return dom.window.document.querySelector("ol").children[ruleIndex]
       .textContent;
   });
