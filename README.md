6d5c67c9ab8619fe0c17c66eda5622c11b1c6a7b
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-28 14:45:37 -0500

    Reply to integer-parseable messages with textContent of Nth rule at http://www.point83.com/tos/index.php?title=Basic_rules

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ See https://claudiajs.com/tutorials/hello-world-chatbot.html
 
 To update, go to https://dev.groupme.com/bots and run:
 
-    yarn claudia update --configure-groupme-bot
+    yarn claudia update --configure-groupme-bot --timeout 10
--- bot.js
+++ bot.js
@@ -1,5 +1,14 @@
 var botBuilder = require("claudia-bot-builder");
+const { JSDOM } = require("jsdom");
 
 module.exports = botBuilder(function(request) {
-  return "Thanks for sending: " + request.text;
+  const ruleNumber = Number.parseInt(request.text);
+  const ruleIndex = ruleNumber - 1;
+
+  return JSDOM.fromURL(
+    "http://www.point83.com/tos/index.php?title=Basic_rules"
+  ).then(dom => {
+    return dom.window.document.querySelector("ol").children[ruleIndex]
+      .textContent;
+  });
 });
