82881712dbb17173664dfef7fa4849b63e5ee180
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-28 17:22:05 -0500

    Set up only GroupMe endpoint
    See https://github.com/claudiajs/claudia-bot-builder/blob/68b0cb350f583ae6f734771a3e605d7a9659ca75/docs/API.md#selecting-platforms
    
    This commit is best viewed with:
    
        git show -w --color-words=.

--- bot.js
+++ bot.js
@@ -2,25 +2,28 @@ const botBuilder = require("claudia-bot-builder");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
-module.exports = botBuilder(function(request) {
-  const { text } = request;
+module.exports = botBuilder(
+  function(request) {
+    const { text } = request;
 
-  const ruleNumber = Number.parseInt(text.match(/rule (\d+)/i)[1]);
-  const ruleIndex = ruleNumber - 1;
+    const ruleNumber = Number.parseInt(text.match(/rule (\d+)/i)[1]);
+    const ruleIndex = ruleNumber - 1;
 
-  const isNyc = text.match(/nyc/i);
-  const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
-  const rulesUrl = isNyc
-    ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
-    : "http://www.point83.com/tos/index.php?title=Basic_rules";
+    const isNyc = text.match(/nyc/i);
+    const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
+    const rulesUrl = isNyc
+      ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
+      : "http://www.point83.com/tos/index.php?title=Basic_rules";
 
-  return fetch(rulesUrl)
-    .then(res => res.text())
-    .then(body => cheerio.load(body))
-    .then($ => {
-      const rules = $("ol").children();
-      const rule = $(rules[ruleIndex]);
+    return fetch(rulesUrl)
+      .then(res => res.text())
+      .then(body => cheerio.load(body))
+      .then($ => {
+        const rules = $("ol").children();
+        const rule = $(rules[ruleIndex]);
 
-      return replyPrefix + rule.text();
-    });
-});
+        return replyPrefix + rule.text();
+      });
+  },
+  { platforms: ["groupme"] }
+);
