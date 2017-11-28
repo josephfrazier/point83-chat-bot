6fa62f9d744329bff7a826be2f8a2cd05a200752
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-28 15:15:30 -0500

    Format for legibility

--- bot.js
+++ bot.js
@@ -3,13 +3,15 @@ const { JSDOM } = require("jsdom");
 
 module.exports = botBuilder(function(request) {
   const { text } = request;
+
   const ruleNumber = Number.parseInt(text.match(/rule (\d+)/i)[1]);
   const ruleIndex = ruleNumber - 1;
+
   const isNyc = text.match(/nyc/i);
+  const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
   const rulesUrl = isNyc
     ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
     : "http://www.point83.com/tos/index.php?title=Basic_rules";
-  const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
 
   return JSDOM.fromURL(rulesUrl).then(dom => {
     return (
