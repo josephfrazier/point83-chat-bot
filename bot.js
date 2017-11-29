bc2215e429a7e89bd1d34a6fe01182ba35944506
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-28 22:05:36 -0500

    Extract getReplyForText()
    This commit is best viewed with:
    
        git show -w

--- bot.js
+++ bot.js
@@ -2,28 +2,25 @@ const botBuilder = require("claudia-bot-builder");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
-module.exports = botBuilder(
-  function(request) {
-    const { text } = request;
+module.exports = botBuilder(getReplyForText, { platforms: ["groupme"] });
 
-    const ruleNumber = Number.parseInt(text.match(/rule (\d+)/i)[1]);
-    const ruleIndex = ruleNumber - 1;
+function getReplyForText({ text }) {
+  const ruleNumber = Number.parseInt(text.match(/rule (\d+)/i)[1]);
+  const ruleIndex = ruleNumber - 1;
 
-    const isNyc = text.match(/nyc/i);
-    const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
-    const rulesUrl = isNyc
-      ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
-      : "http://www.point83.com/tos/index.php?title=Basic_rules";
+  const isNyc = text.match(/nyc/i);
+  const replyPrefix = `${isNyc ? "NYC " : ""}Rule ${ruleNumber}: `;
+  const rulesUrl = isNyc
+    ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
+    : "http://www.point83.com/tos/index.php?title=Basic_rules";
 
-    return fetch(rulesUrl)
-      .then(res => res.text())
-      .then(body => cheerio.load(body))
-      .then($ => {
-        const rules = $("ol").children();
-        const rule = $(rules[ruleIndex]);
+  return fetch(rulesUrl)
+    .then(res => res.text())
+    .then(body => cheerio.load(body))
+    .then($ => {
+      const rules = $("ol").children();
+      const rule = $(rules[ruleIndex]);
 
-        return replyPrefix + rule.text();
-      });
-  },
-  { platforms: ["groupme"] }
-);
+      return replyPrefix + rule.text();
+    });
+}
