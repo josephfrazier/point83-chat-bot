68dd7d06fd56c0793c6e5bbf97d731ae44cc2031
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-29 01:27:01 -0500

    Add console.time() calls for profiling
    For AWS Lambda deployments, these can be seen as described here:
    
    http://docs.aws.amazon.com/lambda/latest/dg/monitoring-functions-logs.html

--- bot.js
+++ bot.js
@@ -14,13 +14,24 @@ function getReplyForText({ text }) {
     ? "http://www.point83.com/tos/index.php?title=Basic_Rules_(NYC_Addendum)"
     : "http://www.point83.com/tos/index.php?title=Basic_rules";
 
+  console.time("fetch");
   return fetch(rulesUrl)
     .then(res => res.text())
-    .then(body => cheerio.load(body))
+    .then(body => {
+      console.timeEnd("fetch");
+
+      console.time("cheerio.load");
+      const result = cheerio.load(body);
+      console.timeEnd("cheerio.load");
+
+      return result;
+    })
     .then($ => {
+      console.time("rule text");
       const rules = $("ol").children();
       const rule = $(rules[ruleIndex]);
       const ruleText = rule.text();
+      console.timeEnd("rule text");
 
       return replyPrefix + ruleText;
     });
